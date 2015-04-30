239ad077126bff2155a740db8aeb2ec874a4a72c
SourenP <souren@papazian.info>
2015-04-29 21:52:59 -0400

    test

--- src/ptest.cpp
+++ src/ptest.cpp
@@ -6,7 +6,7 @@
 using namespace std;
 
 void random_move(Point::Point_p p, Control<Point>& c) {
-    vector<Point::Point_p> elements = c.qneighbors(p, 400);
+    vector<Point::Point_p> elements = c.qneighbors(p, 50);
     p->set_x(c.random_pos(0,800));
     p->set_y(c.random_pos(0,800));
 }
@@ -25,16 +25,16 @@ int main()
     int b = s->CreateBehavior(pattern);
 
     // Create two Points in the middle of the screen facing opposite directions
-    s->CreateRandElements(300, 10, 800, 10, 800, b);
+    s->CreateRandElements(100, 10, 800, 10, 800, b);
 
     // Run this only for testing logic (NOT FOR MEASURE)
-    //Renderer<Point> twodee = Renderer<Point>(800, 800, 1);
-    //s->Run(0,twodee);
+    Renderer<Point> twodee = Renderer<Point>(800, 800, 1);
+    s->Run(20, twodee);
 
     // Run this for measure
-    int step = 100;
+    //int step = 100;
     // Start timing
-    s->Run(step);
+    //s->Run(step);
     // End timing
 
     delete s;
