be00d62ec0309c31fe8f0daf4bbb371d11583dd5
SourenP <souren@papazian.info>
2015-04-30 20:03:02 -0400

    documented all code but point and dot

--- src/dtest.cpp
+++ src/dtest.cpp
@@ -1,11 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
 #include <iostream>
-#include "Point.h"
+#include <cmath>
 #include "Dot.h"
 #include "Simulation.h"
-#include <cmath>
 using namespace std;
 
-int collision_count = 0;
+// Helper functions
+
+/*
+    Returns a random float in the range [min, max) 
+*/
+float random_pos(int min, int max) {
+    return(float) (rand() % max + min);
+}
+
+/*
+    ???
+*/
 float delta_dir(float curr_dir, float goal_dir, float steps) {
     float delta1 = goal_dir - curr_dir;
     float delta2 = (goal_dir > curr_dir) ? (delta1 - 360) : (delta1 + 360);
@@ -15,9 +27,14 @@ float delta_dir(float curr_dir, float goal_dir, float steps) {
         return delta2/steps;
 }
 
+// Boid rules
+
+/*
+    ???
+*/
 void separation(Dot::Dot_p p, Control<Dot>& c) {
     vector<Dot::Dot_p> neighbors = c.qneighbors(p, 50);
-    float steps = 10;
+    float steps = 15;
 
     if(neighbors.size()) {
         float avg_x = c.avg_x(neighbors);
@@ -28,6 +45,9 @@ void separation(Dot::Dot_p p, Control<Dot>& c) {
     }
 }
 
+/*
+    ???
+*/
 void alignment(Dot::Dot_p p, Control<Dot>& c) {
     vector<Dot::Dot_p> neighbors = c.qneighbors(p, 100);
     float steps = 10;
@@ -39,9 +59,12 @@ void alignment(Dot::Dot_p p, Control<Dot>& c) {
     }
 }
 
+/*
+    ???
+*/
 void cohesion(Dot::Dot_p p, Control<Dot>& c) {
     vector<Dot::Dot_p> neighbors = c.qneighbors(p, 150);
-    float steps = 10;
+    float steps = 15;
 
     if(neighbors.size()) {
         float avg_x = c.avg_x(neighbors);
@@ -51,6 +74,8 @@ void cohesion(Dot::Dot_p p, Control<Dot>& c) {
         p->add_ang(step_ang);
     }
 }
+
+// Loop Dots that go out of bounds
 
 void bounds(Dot::Dot_p p, Control<Dot>& c) {
     if(p->get_x() <= 0)
@@ -63,12 +88,11 @@ void bounds(Dot::Dot_p p, Control<Dot>& c) {
         p->set_y(1);
 }
 
-
 int main()
 {
     // Brains
     Simulation<Dot>::rule r1 = &cohesion;
-    Simulation<Dot>::rule r2 = &aligckenment;
+    Simulation<Dot>::rule r2 = &alignment;
     Simulation<Dot>::rule r3 = &separation;
     Simulation<Dot>::rule r4 = &bounds;
 
@@ -80,11 +104,14 @@ int main()
     // Create behavior circle
     int b1 = s->CreateBehavior(boid);
 
+    // Define randome seed
+    srand(time(NULL));
     // Create two Points in the middle of the screen facing opposite directions
-    s->CreateElement(Dot(400, 400, 0, 1, b1));
+    for(int i=0; i < 200; i++)
+        s->CreateElement(Dot(random_pos(0,800), random_pos(0,800), random_pos(0,360), 1, b1));
 
     // Initialize Renderer
-    Renderer<Dot> twodee = Renderer<Dot>(800, 800, 100);
+    Renderer<Dot> twodee = Renderer<Dot>(800, 800, 200);
 
     // Run Simulation for 5000 steps
     s->Run(5000, twodee);
